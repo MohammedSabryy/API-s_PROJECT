c7c6297df08433a39c712a8b89d2b76867208af3
MohamedSabryy <mhmdasabry@gmail.com>
2024-10-03 21:30:40 +0300

    Video 5

---
+++ E-Commerce/Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+    internal class ProductWithBrandAndTypeSpecifications : Specifications<Product>
+    {
+        public ProductWithBrandAndTypeSpecifications(int id)
+            : base(Product=>Product.Id == id)
+        {
+            AddInclude(Product => Product.ProductBrand);
+            AddInclude(Product => Product.ProductType);
+
+        }
+
+
+        public ProductWithBrandAndTypeSpecifications()
+           : base(null)
+        {
+            AddInclude(Product => Product.ProductBrand);
+            AddInclude(Product => Product.ProductType);
+
+        }
+    }
+}
