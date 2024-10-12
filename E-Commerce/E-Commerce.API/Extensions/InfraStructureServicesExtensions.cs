9471faac8c3d632842fdf8b076488feca92ef84c
MohamedSabryy <mhmdasabry@gmail.com>
2024-10-12 17:05:48 +0300

    Video 5

--- E-Commerce/E-Commerce.API/Extensions/InfraStructureServicesExtensions.cs
+++ E-Commerce/E-Commerce.API/Extensions/InfraStructureServicesExtensions.cs
@@ -13,6 +13,7 @@ namespace E_Commerce.API.Extensions
         {
             services.AddScoped<IDbInitializer, DbInitializer>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IBasketRepository, BasketRepository>();
 
             services.AddDbContext<StoreContext>(options =>
             {
