60baa756f0daf703260e10aa9f055372af6c7b20
MohamedSabryy <mhmdasabry@gmail.com>
2024-10-12 17:27:36 +0300

    Video 6

--- E-Commerce/Core/Services/ServiceManager.cs
+++ E-Commerce/Core/Services/ServiceManager.cs
@@ -9,10 +9,15 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IProductService> _productService;
-        public ServiceManager(IUnitOfWork unitOfWork , IMapper mapper)
+        private readonly Lazy<IBasketService> _lazyBasketService;
+        public ServiceManager(IUnitOfWork unitOfWork , IMapper mapper , IBasketRepository basketRepository)
         {
-            _productService =new Lazy<IProductService>(() => new ProductService(unitOfWork, mapper));   
+            _productService =new Lazy<IProductService>(() => new ProductService(unitOfWork, mapper));
+            _lazyBasketService = new Lazy<IBasketService>
+                (() => new BasketService(basketRepository, mapper));
         }
         public IProductService ProductService => _productService.Value;
+
+        public IBasketService BasketService => _lazyBasketService.Value;
     }
 }
